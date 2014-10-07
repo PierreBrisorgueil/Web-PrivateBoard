c142cfd9fbe6db27db876f65bb9d17a341d65c76
Pierre <pierre.brisorgueil@me.com>
2014-10-07 23:25:12 +0200

    Fix Delete File

--- www/index_load_file.php
+++ www/index_load_file.php
@@ -53,6 +53,28 @@ if($cron_file != 1)
 			$folder = $way.$name[$j];
 			tri_folderv4($folder, true);
 		}
+
+		$resultats=$connexion->query("SELECT * FROM files"); 
+		$resultats->setFetchMode(PDO::FETCH_OBJ);
+
+		while( $ligne = $resultats->fetch() ) 
+		{
+			$size_way = strlen($way);
+			$url_file = $ligne->path. DIRECTORY_SEPARATOR .$ligne->fichier;
+			$url_file = substr($url_file,$size_way);
+			$url_file = $path_absolu.$url_file;
+			$url_file = str_replace(' ', '%20', $url_file);
+			$test = url_exists($url_file);
+
+			if($test==false)
+			{
+				$_id = $connexion->quote($ligne->id); 
+				$connexion->exec("DELETE FROM files WHERE id = $_id") or die(print_r($connexion->errorInfo()));	
+			}
+		}
+		$resultats->closeCursor();
+
+		
 		$id = $connexion->quote($_SESSION['id']); 
 		$time = $connexion->quote(time());  
 		$requete2 = $connexion->exec("UPDATE users SET LastLoadFile = $time WHERE id = $id");	
